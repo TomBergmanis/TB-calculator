c62776e227cbc9af6bc43a0ca327c3fe7c7b696a
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2023-11-04 17:21:38 +0000

    adding in the display and working buttons

--- main.js
+++ main.js
@@ -1,26 +1,62 @@
+// create a function that populate the display when you click the number buttons
+const calculatorDisplay = document.querySelector(".display");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsbutton = document.getElementById("#equalsBtn");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+
+// AC function
+allClearButton.addEventListener("click", clearDisplay);
+function clearDisplay() {
+  calculatorDisplay.textContent = "";
+}
+
+// display numbers in calculator screen
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.textContent);
+    calculatorDisplay.textContent += button.textContent;
+  });
+});
+
+// display operations in calculator screen
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.textContent);
+    calculatorDisplay.textContent += button.textContent;
+  });
+});
+
 // add basic
 function add(x, y) {
   return x + y;
 }
-console.log(add(0, 0));
+//console.log(add(0, 0));
 
 // subtract basic
 function subtract(x, y) {
   return x - y;
 }
-console.log(subtract(0, 0));
+// console.log(subtract(0, 0));
 
 // multiply basic
 function multiply(x, y) {
   return x * y;
 }
-console.log(multiply(0, 0));
+// console.log(multiply(0, 0));
 
 // divide basic
 function divide(x, y) {
   return x / y;
 }
-console.log(divide(0, 0));
+// console.log(divide(0, 0));
 
 // operator buttons]
 
@@ -48,4 +84,4 @@ function operate(operator, x, y) {
       return null;
   }
 }
-console.log(operate(1, 5));
+// console.log(operate(1, 5));
